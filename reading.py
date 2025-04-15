edf96fd1363aa60cf9c23b2d4ba8fbdc8be2df76
margarit.alecsandru@gmail.com <margarit.alecsandru@gmail.com>
2025-04-15 21:41:47 +0300

    adding snowflake.ini (hidden)

--- reading.py
+++ reading.py
@@ -1,4 +1,16 @@
 import pandas as pd
+from snowflake.snowpark import Session
 
-df = pd.read_csv("inputs/netflix_titles.csv").head()
-print(df)
+df = pd.read_csv("inputs/netflix_titles.csv")
+top_movies = df[df["type"] == "Movie"]
+print(top_movies)
+
+connection_parameters = {
+    "account": "your_account",
+    "user": "your_user",
+    "password": "your_password",
+    "role": "your_role",
+    "warehouse": "your_warehouse",
+    "database": "your_database",
+    "schema": "your_schema",
+}
